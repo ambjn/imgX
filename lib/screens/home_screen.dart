10ed300fe30d128cafa19f1dce943ae77e49006d
Amber Jain <jainanuamber@gmail.com>
2022-05-25 14:04:36 +0530

    imgX App

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:imgx/screens/edit_image_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: IconButton(
+        icon: const Icon(Icons.upload_file),
+        onPressed: () async {
+          XFile? file = await ImagePicker().pickImage(
+            source: ImageSource.gallery,
+          );
+          if (file != null) {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) =>
+                    EditImageScreen(selectedImage: file.path)));
+          }
+        },
+      )),
+    );
+  }
+}
