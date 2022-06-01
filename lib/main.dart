359ac72c8b95a9b4823bfe0ea1a8db7770244f5f
Amber Jain <jainanuamber@gmail.com>
2022-06-01 23:47:19 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'imgX',
       theme: ThemeData.light(),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,20 +11,55 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: IconButton(
-        icon: const Icon(Icons.upload_file),
-        onPressed: () async {
-          final ImagePicker _picker = ImagePicker();
-          XFile? image = await _picker.pickImage(
-            source: ImageSource.gallery,
-          );
-          if (image != null) {
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (_) => EditImageScreen(selectedImage: image.path)));
-          }
-        },
-      )),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'img',
+                  style: TextStyle(color: Colors.blueAccent, fontSize: 30),
+                ),
+                Text(
+                  'X',
+                  style: TextStyle(color: Colors.grey, fontSize: 32),
+                )
+              ],
+            ),
+          )),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 400,
+          ),
+          const Text("press the button & select photo 🤳"),
+          const SizedBox(
+            height: 10,
+          ),
+          Center(
+              child: IconButton(
+            icon: const Icon(
+              Icons.upload_file,
+              size: 30,
+            ),
+            onPressed: () async {
+              final ImagePicker _picker = ImagePicker();
+              XFile? image = await _picker.pickImage(
+                source: ImageSource.gallery,
+              );
+              if (image != null) {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (_) =>
+                        EditImageScreen(selectedImage: image.path)));
+              }
+            },
+          )),
+        ],
+      ),
     );
   }
 }
