f6b3eb0ab93a9dc0095372367d386a50f88dfd1b
Amber Jain <jainanuamber@gmail.com>
2022-06-01 16:44:49 +0530

    added default_button.dart

--- lib/widgets/edit_image_view_model.dart
+++ lib/widgets/edit_image_view_model.dart
@@ -2,7 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:imgx/screens/edit_image_screen.dart';
 
 abstract class EditImageViewModel extends State<EditImageScreen> {
+  TextEditingController textEditingController = TextEditingController();
   addNewDialog(context) {
-    showDialog(context: context, builder: (context) => AlertDialog());
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Center(child: Text("add new text")),
+              content: TextField(
+                controller: textEditingController,
+                maxLines: 5,
+                decoration: const InputDecoration(
+                    suffixIcon: Icon(Icons.edit),
+                    filled: true,
+                    hintText: 'your text goes here..'),
+              ),
+              actions: [],
+            ));
   }
 }
